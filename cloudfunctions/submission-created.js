e263f05ae33b02f6f78252346e980f35ec18f1e3
cmcg <cody@techlook.io>
2020-02-17 18:45:27 -0800

    api

--- cloudfunctions/submission-created.js
+++ cloudfunctions/submission-created.js
@@ -9,99 +9,65 @@
 // };
 // sgMail.send(msg);
 
-const client = require("@sendgrid/client");
-
-function addSendgridRecipient(client, email) {
-  return new Promise((fulfill, reject) => {
-    const data = [{
-      email: email
-    }];
-    const request = {
-      method: "POST",
-      url: "/v3/contactdb/recipients",
-      body: data
-    };
-
-    client
-      .request(request)
-      .then(([response, body]) => {
-        console.log(response.statusCode);
-        console.log(body);
-        fulfill(response);
-        // cb(null, response);
-      })
-      .catch(error => reject(error));
-  });
-}
-
-function sendWelcomeEmail(client, email, senderEmail, senderName, templateID) {
-  return new Promise((fulfill, reject) => {
-    const data = {
-      from: {
-        email: senderEmail,
-        name: senderName
-      },
-      reply_to: {
-        email: senderEmail
-      },
-      personalizations: [{
-        to: [{
-          email: email
-        }]
-      }],
-      template_id: templateID
-    };
-
-    const request = {
-      method: "POST",
-      url: "/v3/mail/send",
-      body: data
-    };
-
-    client
-      .request(request)
-      .then(([response, body]) => {
-        console.log(response.statusCode);
-        console.log(body);
-        fulfill(response);
-      })
-      .catch(error => reject(error));
-  });
-}
+const sgMail = require('@sendgrid/mail');
 
 exports.handler = function (event, context, callback) {
-  const {
-    SENDGRID_API_KEY,
-    SENDGRID_WELCOME_SENDER_EMAIL,
-    SENDGRID_WELCOME_SENDER_NAME,
-    SENDGRID_WELCOME_TEMPLATE_ID
-  } = process.env;
+  // Parse the body sent to the function.
   const body = JSON.parse(event.body);
-  const email = body.email;
-  const welcomeEmail = event.queryStringParameters.welcome_email === "true";
+  // Find the conditional value.
+  const type = body.data.type_of_inquiry.trim();
 
-  client.setApiKey(SENDGRID_API_KEY);
-  addSendgridRecipient(client, email)
-    .then((response, body) => {
-      if (welcomeEmail) {
-        sendWelcomeEmail(
-            client,
-            email,
-            SENDGRID_WELCOME_SENDER_EMAIL,
-            SENDGRID_WELCOME_SENDER_NAME,
-            SENDGRID_WELCOME_TEMPLATE_ID
-          )
-          .then(response => callback(null, {
-            statusCode: response.statusCode,
-            body: email + " added"
-          }))
-          .catch(err => callback(err, null));
-      } else {
-        callback(null, {
-          statusCode: response.statusCode,
-          body: ""
-        });
-      }
+  // The list of potential email addresses to use.
+  const emails = {
+    Marketing: 'cody@flamingo.video',
+    Sales: 'cody@flamingo.video'
+  };
+
+  // This is the data coming from the form. This is specific to Netlify forms.
+  const dataArray = Object.entries(body.human_fields);
+  // Use that data to build a series of <tr> and <td> (table rows and columns)
+  // for each field in the form.
+  //
+  // Note: I'm doing this so the email recipient can see the contents of the
+  // form and respond directly via email rather than having to go to the source
+  // to find the form contents.
+  const tableData = dataArray
+    .map(x => `<tr><td>${x[0]}</td><td>${x[1]}</td></td>`)
+    .join('');
+  // Wrap the field data in a table so it will render properly in email clients.
+  //   const html = `<table><tbody>${tableData}</tbody></table>`;
+  //   // Build a text version of the contents, as well.
+  //   const text = dataArray.map(x => `${x[0]}: ${x[1]};`).join('');
+
+  // The message object contains the information to pass to SendGrid to send the
+  // appropriate email message.
+  const msg = {
+    to: 'cody@flamingo.video',
+    from: 'cody@flamingo.video',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+
+  // Set the SendGrid API key.
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+  // Send the message.
+  sgMail
+    .send(msg)
+    .then(() => {
+      // If the message was successfully sent, we log the object to the console.
+      // This enables us to see what was sent directly in the Netlify logs.
+      console.log(msg);
+      // The callback in this form tells the service initiating this function
+      // that it was successful.
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(msg),
+      });
     })
-    .catch(err => callback(err, null));
+    // If the message was not successfully sent, then we catch the error and
+    // render it to the logs. This also tells the service that intitiated this
+    // function that it was not successful.
+    .catch(error => callback(error));
 };
